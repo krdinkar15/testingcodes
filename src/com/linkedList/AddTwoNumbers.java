82218e60df277d22b12aff7182be5680e9a225cb
kamal.dinkar <kamal.dinkar@snapdeal.com>
2023-02-21 20:26:56 +0530

    Add two numbers

---
+++ src/com/linkedList/AddTwoNumbers.java
@@ -0,0 +1,77 @@
+package com.linkedList;
+// Problem link : https://leetcode.com/problems/add-two-numbers/
+public class AddTwoNumbers
+{
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode(0);
+        ListNode head = null;
+        int carry = 0;
+        while (l1 != null && l2 != null) {
+            int t = l1.val + l2.val + carry;
+            carry = (t >= 10 )? 1 : 0;
+            t = t % 10;
+            ans.next = new ListNode(t);
+            ans = ans.next;
+            if (head == null)
+                head = ans;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        while (l1 != null) {
+            int t = l1.val + carry;
+            carry = (t >= 10) ? 1 : 0;
+            t = t % 10;
+            ans.next = new ListNode(t);
+            ans = ans.next;
+            if (head == null)
+                head = ans;
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            int t = l2.val + carry;
+            carry = (t >= 10) ? 1 : 0;
+            t = t % 10;
+            ans.next = new ListNode(t);
+            ans = ans.next;
+            if (head == null)
+                head = ans;
+            l2 = l2.next;
+        }
+        if (carry == 1) {
+            ans.next = new ListNode(1);
+            if (head == null)
+                head = ans;
+            ans = ans.next;
+        }
+        return head;
+
+    }
+
+    public static void main(String[] args)
+    {
+        ListNode l1=new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9)))))));
+        ListNode l2=new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9))));
+        ListNode ans=addTwoNumbers(l1,l2);
+        while(ans!=null)
+        {
+            System.out.print(ans.val+" ");
+            ans=ans.next;
+        }
+    }
+}
+class ListNode
+{
+    int val;
+    ListNode next;
+    ListNode(){};
+    ListNode(int val)
+    {
+        this.val=val;
+    }
+    ListNode(int val,ListNode next)
+    {
+        this.val=val;
+        this.next=next;
+    }
+}
