6b939889e6150a82d20775bc687df4b7e914b5b8
kamal.dinkar <kamal.dinkar@snapdeal.com>
2023-03-14 00:29:24 +0530

    valid ValidParentheses

---
+++ src/com/stack/ValidParentheses.java
@@ -0,0 +1,62 @@
+package com.stack;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static boolean checkBracket(char x)
+    {
+        if(x=='{' || x== '[' || x=='(')
+            return true;
+        return false;
+    }
+    public static boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char temp=s.charAt(i);
+            if(checkBracket(temp))
+            {
+                st.push(temp);
+            }
+            else
+            {
+                if(st.size()==0)
+                    return false;
+                else
+                {
+                    char p=st.pop();
+                    if(s.charAt(i)==')')
+                    {
+                        if(p=='(')
+                            continue;
+                        else
+                            return false;
+                    }
+                    else if(s.charAt(i)=='}')
+                    {
+                        if(p=='{')
+                            continue;
+                        else
+                            return false;
+                    }
+                    else if(s.charAt(i)==']')
+                    {
+                        if(p=='[')
+                            continue;
+                        else
+                            return false;
+                    }
+                }
+            }
+        }
+        if(st.size()==0)
+            return true;
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        String str="()[]{}";
+        System.out.println(isValid(str));
+    }
+}
