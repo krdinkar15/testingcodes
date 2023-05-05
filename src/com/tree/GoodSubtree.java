069baa36cf586dede74154b0a5bcb26ecafa1c98
krdinkar15 <krdinkar15@gmail.com>
2023-05-05 13:18:57 +0530

    GoodSubtree

---
+++ src/com/tree/GoodSubtree.java
@@ -0,0 +1,35 @@
+package com.tree;
+
+import com.leetcode.classes.TreeNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// question Name : Good Subtrees
+// https://practice.geeksforgeeks.org/problems/df12afc57250e7f6fc101aa9c272343184fe9859/1
+public class GoodSubtree {
+    static int cnt=0;
+    public static Set<Integer> findGoodSubtree(TreeNode root, int k)
+    {
+
+        if(root==null)
+            return new HashSet<>();;
+        Set<Integer> sleft=findGoodSubtree(root.left,k);
+        Set<Integer> sright=findGoodSubtree(root.right,k);
+        Set<Integer> temp=new HashSet<>();
+        temp.add(root.val);
+        temp.addAll(sleft);
+        temp.addAll(sright);
+        if(temp.size()<=k)
+            cnt++;
+        return temp;
+    }
+    public static int goodSubtrees(TreeNode root,int k)
+    {
+        if(root==null)
+            return 0;
+        cnt=0;
+        findGoodSubtree(root,k);
+        return cnt;
+    }
+}
